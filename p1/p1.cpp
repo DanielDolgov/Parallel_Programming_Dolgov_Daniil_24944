1e266b2b528c986ac61dadc3a6a865ae89117280
DanielDolgov <d.dolgov1@g.nsu.ru>
2026-02-10 11:55:10 +0700

    Задача 1

---
+++ p1/p1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+
+int main() {
+    #ifdef DOUBLE
+        double sum = 0;
+        std::vector<double> sins;
+        std::string type = "Сумма double: ";
+    #else
+        float sum = 0;
+        std::vector<float> sins;
+        std::string type = "Сумма float: ";
+    #endif
+
+    int cnt = pow(10, 7);
+    for (int i = 0; i < cnt; i++) {
+        sins.push_back(sin(2 * M_PI * i / cnt));
+        sum += sins.back();
+    }
+    std::cout << type << sum << std::endl; 
+    return 0;
+}
